631a37f69e1d09b3b23f3d57477e72d25e6c29d6
marcuspereiradev <marcus.work@hotmail.com>
2019-03-20 17:34:09 -0300

    Improving layout with description and icons

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,22 @@
 class ArticlesController < ApplicationController
-    def new
-        
-    end
-    
-    def create
-        @user = User.new(user_params)
-        @user.save
-        redirect_to @user
-    end
+  def new
+      
+  end
+  
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to @user
+  end
 
-    def show
-        @user = User.find(params[:id])
-    end
-    
-    
-    private
-    def user_params
-        params.require(:user).permit(:name, :website, :title, 
-                                     :description, :location, :email)
-    end
-    
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :website, :title, 
+                                  :description, :location, :email)
+  end
 end
--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,6 +1,11 @@
 class RegisterController < ApplicationController
-    def index
-        @users = User.all
+  def index
+    @users = User.all
+    if params[:id]
+      @selected_user = User.find(params[:id])
+    else
+      @selected_user = User.last
     end
-    
+  end
 end
+
