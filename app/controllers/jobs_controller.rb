b212fa14134d92265af7048c856fcb143dc9fe4d
Marcus Pereira <marcus.work@hotmail.com>
2020-12-23 16:25:01 -0300

    Add edit job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,15 +11,30 @@ class JobsController < ApplicationController
   end
 
   def new
-    @jobs = current_user.jobs.build
+    @job = current_user.jobs.build
   end
   
+  def edit
+    @job = Job.find(params[:id])
+  end
+
   def create
-    @jobs =  current_user.jobs.build(job_params)
-    if @jobs.save
+    @job =  current_user.jobs.build(job_params)
+    if @job.save
       redirect_to jobs_path 
     else
       render 'new'
+    end
+  end
+  
+  def update
+    @job = Job.find(params[:id])
+    if @job.update_attributes(job_params)
+      flash[:success] = "Object was successfully updated"
+      redirect_to jobs_url
+    else
+      flash[:error] = "Something went wrong"
+      render 'edit'
     end
   end
 
