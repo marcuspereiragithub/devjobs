7bdcc85859c2c0d5644192a35f64f13db0530b7e
Marcus Pereira <marcus.work@hotmail.com>
2020-12-23 19:14:58 -0300

    Improving some style and config seed to record some jobs

--- Gemfile
+++ Gemfile
@@ -55,6 +55,9 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+
+  # Specific gems
+  gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+20.times do |n|
+  Job.create!(
+    name: Faker::Company.name,
+    website: Faker::Internet.url,
+    title: Faker::Job.title,
+    description: Faker::Lorem.paragraph(sentence_count: 40),
+    location: Faker::Address.city,
+    email: Faker::Internet.email,
+    user_id: rand(1..2)
+  )
+end
