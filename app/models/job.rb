bb4acaa28fdac9789d1da7844b3c779c401b901e
Marcus Pereira <marcus.work@hotmail.com>
2020-12-25 19:59:02 -0300

    Leaving messages with devise

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,9 +1,11 @@
 class Job < ApplicationRecord
   belongs_to :user
-  validates_presence_of :name, message: "'Nome da empresa' não pode ficar em branco"
-  validates_presence_of :title, message: "'Título da vaga' não pode ficar em branco"
-  validates_presence_of :location, message: "'Local' não pode ficar em branco"
-  validates_presence_of :description, message: "'Descrição da vaga' não pode ficar em branco"
+  validates_presence_of :name
+  validates_presence_of :website
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :location
+  validates_presence_of :email
   
   def first_letter
     return '#' unless name?
